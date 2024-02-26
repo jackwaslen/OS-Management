17d9f878e5a240b659bf0471518e0f2f8a4def00
Jack <jackwaslen16@gmail.com>
2024-02-26 11:16:08 -0500

    Create thread_management.c

---
+++ thread_management.c
@@ -0,0 +1,110 @@
+/*
+ -------------------------------------
+ Assignment 4
+ File: resource_managment.c 
+ -------------------------------------
+ Author:  Connor Doidge and Jack Waslen
+ ID:      210279850 and 210468930
+ Email:   doid9850@mylaurier.ca and wasl8390@mylaurier.ca
+ Version  2023-11-11
+ -------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAX_RESOURCES 5
+int available_resources = MAX_RESOURCES;
+pthread_mutex_t lock;
+
+// 0 is false, 1 true
+int available = 1;
+#define NUM_THREADS 5
+
+/* decrease available_resources by count resources */
+/* return 0 if sufficient resources available*/
+/* otherwise return -1 */
+int decrease_count(int thread_number, int count){
+    if (available_resources < count){
+        return -1;
+    } else {
+        available_resources -= count;
+        printf("The thread %d has acquired, %d resources and %d more resources are available. \n", thread_number, count, available_resources);
+        return 0;
+    }
+}
+
+/* increase available_resources by count*/
+int increase_count(int thread_number, int count){
+    available_resources += count;
+    printf("The thread %d has released, %d resources and %d resources are now available. \n",thread_number, count, available_resources);
+    return 0;
+}
+
+
+void* thread_function(void* THREAD_NUMBER){
+    int thread = *((int *) THREAD_NUMBER);
+    free(THREAD_NUMBER);
+   // printf("Thread #: %d\n", thread);
+    int result = -1;
+    if(pthread_mutex_lock(&lock) == 0){
+       // printf("Lock is : %d, decreasing count\n",0);
+        result = decrease_count(thread, 1);
+       // printf("Result is %d\n", result);
+        pthread_mutex_unlock(&lock);
+    }
+
+
+
+    if (result == 0){
+       
+        sleep(1);
+        if (pthread_mutex_lock(&lock) == 0){
+            //printf("Lock is : %d, increasing count\n",0);
+           
+            increase_count(thread, 1);
+            pthread_mutex_unlock(&lock);
+        }
+       // pthread_mutex_lock(&lock);
+   
+        //pthread_mutex_unlock(&lock);
+
+    } else {
+        printf("Thread %d could not acquire enough resources.\n",thread);
+    }
+  
+    return 0;
+    
+}
+int main(){
+    // call function void* thread_function (THREAD_NUMBER)
+   // invokes decrease count, if succeeded in request, sleep for 1 second otherwise
+   // print "Thread NUMBER could not acquire enough resources"
+   // after 1s thread returns resources by calling increase_count
+   //pthread_mutex_init(&lock, NULL);
+   int threadCount = 0;
+   pthread_t *threadArray;
+   threadArray = malloc(100 * sizeof(*threadArray));
+   while (threadCount < NUM_THREADS){
+        pthread_t t_id;
+        threadArray[threadCount] = t_id;
+        pthread_mutex_init(&lock, NULL);
+        int *arg = malloc(sizeof(*arg));
+        //printf("Thread #: %d\n", threadCount);
+        *arg = threadCount;
+        pthread_create(&t_id, NULL, thread_function, arg);
+     
+    
+        threadCount += 1;
+   }
+   threadCount = 0;
+   while (threadCount < NUM_THREADS){
+      pthread_join(threadArray[threadCount], NULL);
+      threadCount +=1;
+   }
+   
+    printf("All threads have finished execution. Available resources: %d\n", available_resources);
+    exit(0);
+}
